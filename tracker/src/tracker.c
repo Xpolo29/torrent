8ddf7cede0450bd469711b596a79f9bb35cb0dc2
Paul GUYENNET <pguyennet@enseirb.fr>
2024-03-11 18:57:08 +0100

    tracker arg parsing + config reading

--- tracker/src/tracker.c
+++ tracker/src/tracker.c
@@ -1,6 +1,14 @@
 #include "tracker.h"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+//global var
+char* config_path = "config.ini";
+int log_level = -1;
+int16_t port = -1;
+
 
 int compare(struct data *in, struct data *out, long filesize, enum op_t op,
             int len) {
@@ -42,4 +50,136 @@ int filter(struct data *list, char *filename, long filesize, enum op_t op) {
   }
 }
 
-int main(){};
+int parse_args(int argc, char** argv){
+	for(int i = 1; i < argc; ++i){
+		for(int j = 0; j < LEN_ARGS; ++j){
+			if(strcmp(ARGS[j], argv[i]) == 0){
+				switch(j){
+					case 0: // -v
+					case 1: // --verbose
+						// check if there is a value
+						if(i + 1 < argc && isdigit(argv[i+1][0])){
+							log_level = atoi(argv[i + 1]) % 4;
+							++i;
+						}else{
+							if(log_level == -1){log_level = 2;}
+							else{log_level =(log_level + 1) % 4;}
+						}
+						break;
+					case 2: // -h
+					case 3: // --help
+						printf("help\n");
+						break;
+					case 4: // -p
+					case 5: // --port
+						if(i + 1 < argc && isdigit(argv[i+1][0])){
+							port = atoi(argv[i + 1]);
+							++i;
+						}else{
+							printf("No port specified, using default\n");
+						}
+						break;
+					case 6: // -c
+					case 7: // --config
+						if(i + 1 < argc){
+							config_path = argv[i + 1];
+							++i;
+						}else{
+							printf("No path specified, using default\n");
+						}
+						break;
+					default:
+						break;
+				}
+				break;
+
+			} else if (j+1 == LEN_ARGS){
+				printf("Unknown arg error\n");
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int apply_parameter(char* key, char* value){
+	const char key_arr[3][16] = {
+		"port", "verbose", " "	
+	};
+
+	int i = 0;
+	while(key_arr[i][0] != ' '){
+		if(strcmp(key, key_arr[i]) == 0){
+			switch(i){
+				case 0: //port
+					if(port == -1)
+					port = atoi(value);
+					break;
+				case 1: //verbose
+					if(log_level == -1)
+					log_level = atoi(value);
+					break;
+				default:
+					break;
+			}
+		}
+		++i;	
+	}
+	return 0;
+}
+
+int load_config(char* path){
+	// Open the config file for reading
+	FILE *file = fopen(path, "r");
+	if (file == NULL) {
+		perror("Error opening file");
+		return 1;
+	}
+
+	char line[64];
+	char section[64];
+	char key[64];
+	char value[64];
+
+	// Read the file line by line
+	while (fgets(line, sizeof(line), file) != NULL) {
+		// Trim leading and trailing whitespace
+		char *trimmed_line = strtok(line, "\r\n");
+
+		// Skip empty lines
+		if (trimmed_line[0] == '\0')continue;
+
+		// Check if this line represents a section
+		if (trimmed_line[0] == '[') {
+			// Extract section name
+			//sscanf(trimmed_line, "[%[^]]", section);
+			//printf("Section: %s\n", section);
+		} else {
+			// Parse key-value pairs
+			sscanf(trimmed_line, "%[^=] = %[^\n]", key, value);
+			//printf("reading %s:%s\n", key, value);
+			if(apply_parameter(key, value))return 1;
+		}
+	}
+
+	// Close the file
+	fclose(file);
+	return 0;
+}
+
+int main(int argc, char** argv){
+	//parsing args
+	if(parse_args(argc, argv))return 1;
+
+	//loading config
+	if(load_config(config_path))return 2;	
+
+	//printing current values
+	printf("Path to config is %s\n", config_path);
+	printf("Log level set to %d\n", log_level);
+	printf("Currently listening to port %d\n", port);
+
+
+
+};
+
--- tracker/src/tracker.h
+++ tracker/src/tracker.h
@@ -1,5 +1,5 @@
 #include "database.h"
-
+#define LEN_ARGS 8
 enum request_t { announce = 0, look, getfile, update };
 
 enum op_t { eq, gt, lt };
@@ -7,3 +7,10 @@ enum op_t { eq, gt, lt };
 int filter(struct data *list, char *filename, long filesize, enum op_t op);
 
 int load_config(char *filepath);
+
+static const char ARGS[LEN_ARGS][16] = {
+	"-v", "--verbose",
+       	"-h", "--help",
+       	"-p", "--port",
+       	"-c", "--config" 
+};
