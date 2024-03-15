56a95704807c0bc38c96502c2e01868fbb852ef5
APARICIO Mathias <maparicio001@enseirb.fr>
2024-03-15 12:12:58 +0100

    commit de fin de séance

--- peer/rust-client/src/com.rs
+++ peer/rust-client/src/com.rs
@@ -1,44 +1,36 @@
 // src/com.rs
-use std::io::Read;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpStream;
-use crate::config::FileProp;
-enum Message {
-    OK,
-    LIST,
-    ERROR,
-    NONE,
-}
+use crate::config::{FileProp, Message}; // Import enum from config
+use crate::parse::parse_answer;
+
 
 // Wait for the answer of the tracker
-fn goes_well(message: Message, buffer: String) {
+fn goes_well(buffer: String, message: Message) {
     match message {
         Message::OK => {
-            println!("The message was sent successfully");
+            parse_answer(buffer, Message::OK);
         }
         Message::LIST => {
-            println!("The list of files is: {}", buffer);
-        }
-        Message::ERROR => {
-            println!("An error occurred: {}", buffer);
-        }
-        Message::NONE => {
-            // Handle NONE case
+            parse_answer(buffer, Message::LIST);
         }
     }
 }
 
-// FIXME : tracker adress should be taken from the configuration file
-fn send_message(msg: String, _message: Message) {
+fn send_message(msg: String, answer_type: Message) {
     // Connect to the tracker unwrap function takes the Ok variant of the Result and returns the value inside
     // expect() si le résultat est Err, le programme crash avec le message passé en paramètre
-    let mut stream =
-        TcpStream::connect("127.0.0.1:7878").expect("Pas réussis à se connecter au tracker");
+    let ip ="127.0.1";
+    let port = "7878";
+    // Connect to the tracker
+    let mut stream = TcpStream::connect(format!("{}:{}", ip, port)).unwrap();
+    // Send the message
     stream.write(msg.as_bytes()).unwrap();
     // Listen for the answer
     let mut buffer = String::new();
     stream.read_to_string(&mut buffer).unwrap();
-    goes_well(Message::OK, buffer);
+    // Handle awaited answer
+    goes_well(buffer, answer_type);
 }
 
 // Send port and available files to tracker
@@ -64,17 +56,18 @@ pub fn send_port_seed_to_tracker(port: u16, files: Vec<FileProp>) {
         port,
         files_string.join(" ")
     );
-    send_message(msg, Message::NONE);
+    send_message(msg, Message::OK);
 }
 
 // works if there is one criterion
 pub fn send_search_to_tracker(criterions: String) {
     if criterions.is_empty() {
-        send_message("look\r\n".to_string(), Message::NONE);
+        send_message("look\r\n".to_string(), Message::OK);
     } else {
-        send_message(format!("look [filename=\"{}\"]\r\n", criterions), Message::NONE);
+        send_message(format!("look [filename=\"{}\"]\r\n", criterions), Message::OK);
     }
 }
 
 #[cfg(test)]
 mod tests {}
+
--- peer/rust-client/src/main.rs
+++ peer/rust-client/src/main.rs
@@ -2,21 +2,23 @@
 mod com;
 mod config;
 mod userinput;
-
+mod parse;
 use com::{send_port_seed_to_tracker, send_search_to_tracker};
 use config::Config;
 use userinput::{get_available_files, get_listen_port, get_proposed_files};
 
 fn main() {
     let mut config = Config {
+        ip: "127.0.0.1".to_string(),  
         port: 8080,
         files: Vec::new(),
     };
     // nul si le port change pas et en plus ça marche pas
-
     config.port = get_listen_port();
     config.files = get_proposed_files();
     send_port_seed_to_tracker(config.port, config.files);
     let desired_files = get_available_files();
     send_search_to_tracker(desired_files);
+    /*
+    */
 }
