7e17e1891ecf606f229bbcf9e1d34bb5aa90ee74
Mathias <Inutile0@protonmail.com>
2024-03-20 23:56:33 +0100

    Update TCP listener port in mock_tracker.rs and fix formatting in menu.rs

--- peer/client/src/com.rs
+++ peer/client/src/com.rs
@@ -1,10 +1,10 @@
-use crate::data::{TrackerConfig,PeerConfig,MetaFile};
-use log::{info, warn, error};
-use std::net::TcpStream;
+use crate::data::{MetaFile, PeerConfig, TrackerConfig};
+use log::{error, info, warn};
 use std::io::{BufRead, BufReader, Write};
+use std::net::TcpStream;
 /// Takes a list of seeded and leeched files with medata data and returns the right message to be sent
-pub fn seed(seeded: Vec<MetaFile>, peer_port: String, leeched: String) -> String{
-/*
+pub fn seed(seeded: Vec<MetaFile>, peer_port: String, leeched: String) -> String {
+    /*
     into_iter() : transform the vector into an iterator
     map() : apply a function to each element of the iterator
     collect() : transform the iterator into a vector
@@ -25,55 +25,65 @@ pub fn seed(seeded: Vec<MetaFile>, peer_port: String, leeched: String) -> String
         peer_port,
         seeded_string.join(" "),
         leeched
-    ); 
+    );
     msg
 }
 /// Sends a message to a given adress and port
 pub fn send(message: String, port: u16, adress: String) {
-let mut stream = TcpStream::connect(format!("{}:{}", adress, port));
-match stream {
-    Ok(mut stream) => {
-        stream.write(message.as_bytes()).unwrap();
-        info!("Sending to {}:{} < {}", adress, port, message);
+    let mut stream = TcpStream::connect(format!("{}:{}", adress, port));
+    match stream {
+        Ok(mut stream) => {
+            stream.write(message.as_bytes()).unwrap();
+            info!("Sending to {}:{} < {}", adress, port, message);
+        }
+        Err(e) => {
+            error!("Could not connect to tracker: {}", e);
+        }
     }
-    Err(e) => {
-        error!("Could not connect to tracker: {}", e);
-    }
-}
 }
 
 /// Receives a message from a given adress and port
 pub fn receive(expected_answer: &dyn ExpectedAnswer, port: u16, adress: String) -> String {
-    let mut stream = TcpStream::connect(format!("{}:{}", adress, port)).unwrap();
-    let mut buffer = String::new();
-    let mut reader = BufReader::new(&stream);
-    match reader.read_line(&mut buffer) {
-        Ok(_) => {
-            info!("Received from {}:{} > {}", adress, port, buffer);
-            expected_answer.shutdown(&mut stream);
-            return buffer;
+    let mut stream = TcpStream::connect(format!("{}:{}", adress, port));
+    match stream {
+        Ok(mut stream) => {
+            let mut buffer = String::new();
+            let mut reader = BufReader::new(&stream);
+            match reader.read_line(&mut buffer) {
+                Ok(_) => {
+                    info!("Received from {}:{} > {}", adress, port, buffer);
+                    expected_answer.shutdown(&mut stream);
+                    return buffer;
+                }
+                Err(e) => {
+                    error!("Could not receive from tracker: {}", e);
+                    expected_answer.shutdown(&mut stream);
+                    return String::from("");
+                }
+            }
         }
         Err(e) => {
-            error!("Could not receive from tracker: {}", e);
-            expected_answer.shutdown(&mut stream);
+            error!("Could not connect to tracker: {}", e);
             return String::from("");
         }
     }
 }
 
 pub trait ExpectedAnswer {
-    fn check_answer(&self, answer:String) -> Result<String, std::io::Error>;
-    fn retrieve_data(&self, answer:String) -> Result<Answer, std::io::Error>;
+    fn check_answer(&self, answer: &str) -> Result<String, std::io::Error>;
+    fn retrieve_data(&self, answer: String) -> Result<Answer, std::io::Error>;
     fn shutdown(&self, stream: &mut TcpStream);
-    }
-
+}
 
 impl ExpectedAnswer for ExpectOk {
-    fn check_answer(&self, answer: String) -> Result<String, std::io::Error> {
-        if answer == "ok\r\n" {
-            Ok("Réponse correcte du tracker".to_string())
+    fn check_answer(&self, answer: &str) -> Result<String, std::io::Error> {
+        if answer == "ok\n" {
+            Ok("Correct tracker answer".to_string())
         } else {
-            Err(std::io::Error::new(std::io::ErrorKind::Other, "Erreur réponse du tracker"))
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Bad tracker answer",
+            ))
         }
     }
     fn retrieve_data(&self, _answer: String) -> Result<Answer, std::io::Error> {
@@ -83,7 +93,6 @@ impl ExpectedAnswer for ExpectOk {
     fn shutdown(&self, stream: &mut TcpStream) {
         stream.shutdown(std::net::Shutdown::Both).unwrap();
     }
-
 }
 
 pub enum Answer {
@@ -91,15 +100,6 @@ pub enum Answer {
     List(Vec<MetaFile>),
 }
 pub struct ExpectOk;
-
-
-
-
-
-
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -122,6 +122,5 @@ mod tests {
                 hash: String::from("hash2"),
             },
         ];
-        
     }
 }
