8dec2bde6d5f28a7215077fad430eaffbbe2177c
Mathias <Inutile0@protonmail.com>
2024-03-27 19:35:38 +0100

    Add simplelog crate and initialize logging to a file

--- peer/client/src/main.rs
+++ peer/client/src/main.rs
@@ -5,8 +5,11 @@ mod respons_handler;
 mod userinput;
 use data::TrackerConfig;
 use menu::display_menu;
+use simplelog::*;
+use std::fs::File;
 fn main() {
-    env_logger::init();
+    let log_file = File::create("client.log").unwrap();
+    WriteLogger::init(LevelFilter::Trace, Config::default(), log_file).unwrap();
     let tracker_config = TrackerConfig::new();
     display_menu(tracker_config);
 }
