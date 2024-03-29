c5d466f0e635258ebfcc0289cfb70e782aaec1bd
Badr <habti@proton.me>
2024-03-29 11:14:23 +0100

    last db commit :(

--- peer/client/src/db.rs
+++ peer/client/src/db.rs
@@ -24,7 +24,7 @@ impl Database {
     pub async fn get_file(&self, key: &str) -> Result<(String, i32, i32, String), sqlx::Error> {
         let row = sqlx::query(
             r#"
-            SELECT filename, length, piece_size, key
+            SELECT filename, length, piece_size, key, buffersize
             FROM file
             WHERE key = $1
             "#,
@@ -37,16 +37,18 @@ impl Database {
     }
 
     pub async fn insert_file(&self, filename: &str, length: i32, piece_size: i32, key: &str) -> Result<u64, sqlx::Error> {
+        let buffersize = ((length as f32 / piece_size as f32).ceil() as i32) / 8;
         sqlx::query(
             r#"
-            INSERT INTO file (filename, length, piece_size, key)
-            VALUES ($1, $2, $3, $4)
+            INSERT INTO file (filename, length, piece_size, key, buffersize)
+            VALUES ($1, $2, $3, $4, $5)
             "#,
         )
         .bind(filename)
         .bind(length)
         .bind(piece_size)
         .bind(key)
+        .bind(buffersize)
         .execute(&self.pool)
         .await
     }
@@ -62,69 +64,73 @@ impl Database {
         .execute(&self.pool)
         .await
     }
-
-    pub async fn insert_peer(&self, ip: &str, port: i32) -> Result<u64, sqlx::Error> {
-        sqlx::query(
-            r#"
-            INSERT INTO peer (ip, port)
-            VALUES ($1, $2)
-            "#,
-        )
-        .bind(ip)
-        .bind(port)
-        .execute(&self.pool)
-        .await
-    }
-
-    pub async fn get_peer(&self, ip: &str, port: i32) -> Result<(String, i32), sqlx::Error> {
-        let row = sqlx::query(
-            r#"
-            SELECT ip, port
-            FROM peer
-            WHERE ip = $1 AND port = $2
-            "#,
-        )
-        .bind(ip)
-        .bind(port)
-        .fetch_one(&self.pool)
-        .await?;
-
-        Ok((row.get(0), row.get(1)))
-    }
-
-    pub async fn delete_peer(&self, ip: &str, port: i32) -> Result<u64, sqlx::Error> {
-        sqlx::query(
-            r#"
-            DELETE FROM peer
-            WHERE ip = $1 AND port = $2
-            "#,
-        )
-        .bind(ip)
-        .bind(port)
-        .execute(&self.pool)
-        .await
-    }
     
-    pub async fn insert_provide(&self, ip: &str, port: i32, key: &str, buffermap: &[u8]) -> Result<u64, sqlx::Error> {
+    pub async fn insert_peer_no_buffermap(&self, ip: &str, port: i32, key: &str) -> Result<u64, sqlx::Error> {
         sqlx::query(
             r#"
-            INSERT INTO provide (ip, port, key, buffermap)
+            INSERT INTO peer (ip, port, key, buffermap)
             VALUES ($1, $2, $3, $4)
             "#,
         )
         .bind(ip)
         .bind(port)
         .bind(key)
-        .bind(buffermap)
+        .bind(Option::<&[u8]>::None)
+        .execute(&self.pool)
+        .await
+    }
+
+    pub async fn insert_peer_with_buffermap(&self, ip: &str, port: i32, key: &str) -> Result<u64, sqlx::Error> {
+        let row: (i32,) = sqlx::query_as(
+            r#"
+            SELECT buffersize
+            FROM file
+            WHERE key = $1
+            "#,
+        )
+        .bind(key)
+        .fetch_one(&self.pool)
+        .await?;
+    
+        let buffersize = row.0;
+        let buffermap = vec![0; buffersize as usize];
+    
+        sqlx::query(
+            r#"
+            INSERT INTO peer (ip, port, key, buffermap)
+            VALUES ($1, $2, $3, $4)
+            "#,
+        )
+        .bind(ip)
+        .bind(port)
+        .bind(key)
+        .bind(&buffermap)
         .execute(&self.pool)
         .await
     }
     
-    pub async fn get_provide(&self, ip: &str, port: i32, key: &str) -> Result<(String, i32, String, Vec<u8>), sqlx::Error> {
+    pub async fn get_peer(&self, ip: &str, port: i32, key: &str) -> Result<(String, i32, String, Vec<u8>), sqlx::Error> {
         let row = sqlx::query(
             r#"
             SELECT ip, port, key, buffermap
-            FROM provide
+            FROM peer
+            WHERE ip = $1 AND port = $2 AND key = $3
+            "#,
+        )
+        .bind(ip)
+        .bind(port)
+        .bind(key)
+        .fetch_one(&self.pool)
+        .await?;
+    
+        Ok((row.get(0), row.get(1), row.get(2), row.get(3)))
+    }
+
+    pub async fn get_buffermap(&self, ip: &str, port: i32, key: &str) -> Result<(String, i32, String, Vec<u8>), sqlx::Error> {
+        let row = sqlx::query(
+            r#"
+            SELECT buffermap
+            FROM peer
             WHERE ip = $1 AND port = $2 AND key = $3
             "#,
         )
@@ -137,11 +143,24 @@ impl Database {
         Ok((row.get(0), row.get(1), row.get(2), row.get(3)))
     }
     
-    pub async fn delete_provide(&self, ip: &str, port: i32, key: &str) -> Result<u64, sqlx::Error> {
+    pub async fn delete_peer(&self, ip: &str, port: i32, key: &str) -> Result<u64, sqlx::Error> {
         sqlx::query(
             r#"
-            DELETE FROM provide
+            DELETE FROM peer
             WHERE ip = $1 AND port = $2 AND key = $3
+            "#,
+        )
+        .bind(ip)
+        .bind(port)
+        .bind(key)
+        .execute(&self.pool)
+        .await
+    }
+
+    pub async fn update_buffer_peer(&self, ip: &str, port: i32, key: &str, buffermap (size ??)) -> Result<u64, sqlx::Error> {
+        sqlx::query(
+            r#"
+            FIXME OR DELETE ME OR DELTE THE WHOLE FILE LOL
             "#,
         )
         .bind(ip)
@@ -162,21 +181,15 @@ async fn main() -> Result<(), sqlx::Error> {
             filename TEXT,
             length INTEGER,
             piece_size INTEGER,
-            key TEXT PRIMARY KEY
+            key TEXT PRIMARY KEY,
+            buffersize INTEGER
         );
 
         CREATE TABLE IF NOT EXISTS peer (
             ip TEXT,
             port INTEGER,
-            PRIMARY KEY (ip, port)
-        );
-
-        CREATE TABLE IF NOT EXISTS provide (
-            ip TEXT,
-            port INTEGER,
             key TEXT REFERENCES file(key),
             buffermap BYTEA,
-            FOREIGN KEY (ip, port) REFERENCES peer(ip, port),
             PRIMARY KEY (ip, port, key)
         );
         "#,
