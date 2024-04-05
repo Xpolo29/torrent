63569aace864d5b7913bcd2c0476e6ee14b4db32
APARICIO Mathias <maparicio001@enseirb.fr>
2024-04-05 11:27:01 +0200

    Add new structs and trait for TCP communication

--- peer/src/tasks.rs
+++ peer/src/tasks.rs
@@ -1,6 +1,6 @@
 use std::thread::sleep;
 use std::time::Duration; 
-
+use hashbrown::HashMap;
 //task struct
 pub struct Task {
     value : i32,
@@ -16,3 +16,28 @@ impl Task {
         sleep(Duration::from_millis(100));
     }
 }
+
+trait TaskTraitResponse {
+    fn process(self, thread_id : i32);
+}
+
+/// receieved via TCP getpieces and return a data request to be send
+pub struct getpieces {
+    pub key: String,
+    pub pieces: Vec<u32>,
+}
+/// receieved via TCP interested and return a have request to be send
+pub struct interested {
+    pub key: String,
+}
+/// receieved via TCP have and return a interested request to be send
+pub struct have {
+    pub key: String,
+    pub buffermap: Vec<u8>,
+}
+pub struct data {
+    pub key: String,
+    // TODO takes pieces size from config
+    pub pieces: HashMap<u32, Vec<u8>>,
+}
+
